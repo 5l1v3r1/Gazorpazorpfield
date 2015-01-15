ef7f69b582747f9c8a5c72c46013969069b9a37e
Brady Sullivan <brady@bsull.com>
2015-01-15 10:10:16 -0800

    Add calc.py

---
+++ calc.py
@@ -0,0 +1,28 @@
+# Function to add two numbers.
+def add(a, b):
+    return a+b
+
+# Function to subtract two numbers.
+def sub(a, b):
+    return a-b
+
+# Function to multiply two numbers.
+def mult(a, b):
+    return a*b
+
+# Function to divide two numbers.
+def div(a, b):
+    return a/b
+
+
+
+number1 = 1337
+number2 = 69
+
+print("number1 = %d, number2 = %d" % (number1, number2))
+
+c = add(number1, number2)
+
+print("c = %d" % c)
+
+
